4f6ae0c5e162578f57d203c5e24e6d0c26946d62
Marcus <marcus.larson@live.com>
2014-04-01 16:26:16 +0200

    Parralell Julia

--- Våren/Hemuppgift 9/Julia_parrallell.go
+++ Våren/Hemuppgift 9/Julia_parrallell.go
@@ -4,18 +4,29 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"image/png"
 	"log"
 	"math/cmplx"
 	"os"
+	"runtime"
 	"strconv"
+	"sync"
+	"time"
 )
+
+type Stopwatch struct {
+	start, stop time.Time       // no need for lap, see mark
+	mark        time.Duration   // mark is the duration from the start that the most recent lap was started
+	laps        []time.Duration //
+}
 
 type ComplexFunc func(complex128) complex128
 
 var Funcs []ComplexFunc = []ComplexFunc{
+	func(z complex128) complex128 { return cmplx.Sqrt(cmplx.Sinh(z*z)) + complex(0.065, 0.122) },
 	func(z complex128) complex128 { return z*z - 0.61803398875 },
 	func(z complex128) complex128 { return z*z + complex(0, 1) },
 	func(z complex128) complex128 { return z*z + complex(-0.835, -0.2321) },
@@ -23,46 +34,64 @@ var Funcs []ComplexFunc = []ComplexFunc{
 	func(z complex128) complex128 { return z*z*z + 0.400 },
 	func(z complex128) complex128 { return cmplx.Exp(z*z*z) - 0.621 },
 	func(z complex128) complex128 { return (z*z+z)/cmplx.Log(z) + complex(0.268, 0.060) },
-	func(z complex128) complex128 { return cmplx.Sqrt(cmplx.Sinh(z*z)) + complex(0.065, 0.122) },
 }
 
 func main() {
+	wg := new(sync.WaitGroup)
+	wg.Add(len(Funcs))
+	clock := New(0, false) // for measuring the time it takes to produce pictures
+	clock.Start()
 	for n, fn := range Funcs {
-		err := CreatePng("picture-"+strconv.Itoa(n)+".png", fn, 1024)
-		if err != nil {
-			log.Fatal(err)
-		}
+		number := n     // to avoid data race
+		localfunc := fn // to avaoid data race
+		go func() {
+			err := CreatePng("picture-"+strconv.Itoa(number)+".png", localfunc, 1024, wg)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}()
 	}
-	
+	wg.Wait() //Wait for all pictures to be done
+	//clock.Stop()
+	fmt.Print(clock.ElapsedTime()) // prints the time it took for the pictures to be generated
 }
 
 // CreatePng creates a PNG picture file with a Julia image of size n x n.
-func CreatePng(filename string, f ComplexFunc, n int) (err error) {
+func CreatePng(filename string, f ComplexFunc, n int, wg *sync.WaitGroup) (err error) {
 	file, err := os.Create(filename)
 	if err != nil {
 		return
 	}
 	defer file.Close()
 	err = png.Encode(file, Julia(f, n))
+	wg.Done()
 	return
 }
 
 // Julia returns an image of size n x n of the Julia set for f.
 func Julia(f ComplexFunc, n int) image.Image {
+	wgp := new(sync.WaitGroup)
+	wgp.Add(n * n)
 	bounds := image.Rect(-n/2, -n/2, n/2, n/2)
 	img := image.NewRGBA(bounds)
 	s := float64(n / 4)
 	for i := bounds.Min.X; i < bounds.Max.X; i++ {
-		go func () {
+		temp := i
+		go func() {
+
 			for j := bounds.Min.Y; j < bounds.Max.Y; j++ {
-			n := Iterate(f, complex(float64(i)/s, float64(j)/s), 256)
-			r := uint8(0)
-			g := uint8(0)
-			b := uint8(n % 32 * 8)
-			img.Set(i, j, color.RGBA{r, g, b, 255})
-		}
+				temp2 := j
+				n := Iterate(f, complex(float64(temp)/s, float64(temp2)/s), 256)
+				r := uint8(0)
+				g := uint8(0)
+				b := uint8(n % 32 * 8)
+				img.Set(temp, temp2, color.RGBA{r, g, b, 255})
+				wgp.Done()
+			}
+			//wgp.Done()
 		}()
 	}
+	wgp.Wait()
 	return img
 }
 
@@ -76,4 +105,62 @@ func Iterate(f ComplexFunc, z complex128, max int) (n int) {
 		z = f(z)
 	}
 	return
+}
+
+// New creates a new stopwatch with starting time offset by
+// a user defined value. Negative offsets result in a countdown
+// prior to the start of the stopwatch.
+func New(offset time.Duration, active bool) *Stopwatch {
+	var sw Stopwatch
+	sw.Reset(offset, active)
+	return &sw
+}
+
+// Reset allows the re-use of a Stopwatch instead of creating
+// a new one.
+func (s *Stopwatch) Reset(offset time.Duration, active bool) {
+	now := time.Now()
+	s.start = now.Add(-offset)
+	if active {
+		s.stop = time.Time{}
+	} else {
+		s.stop = now
+	}
+	s.mark = 0
+	s.laps = nil
+}
+
+// Active returns true if the stopwatch is active (counting up)
+func (s *Stopwatch) Active() bool {
+	return s.stop.IsZero()
+}
+
+// Stop makes the stopwatch stop counting up
+func (s *Stopwatch) Stop() {
+	if s.Active() {
+		s.stop = time.Now()
+	}
+}
+
+// Start intiates, or resumes the counting up process
+func (s *Stopwatch) Start() {
+	if !s.Active() {
+		diff := time.Now().Sub(s.stop)
+		s.start = s.start.Add(diff)
+		s.stop = time.Time{}
+	}
+}
+
+// Elapsed time is the time the stopwatch has been active
+func (s *Stopwatch) ElapsedTime() time.Duration {
+	if s.Active() {
+		return time.Since(s.start)
+	}
+	return s.stop.Sub(s.start)
+}
+
+// Init sets runtime to be able to run on all CPUs
+func init() {
+	numcpu := runtime.NumCPU()
+	runtime.GOMAXPROCS(numcpu) // Try to use all available CPUs.
 }
